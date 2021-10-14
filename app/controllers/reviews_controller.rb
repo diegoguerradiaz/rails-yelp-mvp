1b1a5b35a7b6bc43e818a8bbbd2abb9d4ab61dd8
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-14 20:30:03 -0300

    added the reviews form to the restaurant show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,20 @@
 class ReviewsController < ApplicationController
 
-  def index
-
-  end
-  def new
-    @review = Review.new
-  end
-
   def create
-    # pending action
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
