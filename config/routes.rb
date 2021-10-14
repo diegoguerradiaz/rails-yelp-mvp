ab7180b65b33d8f3aa09760eec4e1cf183e85f61
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-14 19:40:45 -0300

    fixed reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'restaurants#index'
   resources :restaurants do
+    resources :reviews, only: [:index, :create]
   end
-
-  # If inside the block, this means that it is a NESTED resource
-  # Which means:
-  # It starts with parent_resources/:parent_resources_id/the-normal-route
-  # /restaurants/:restaurant_id/reviews
-  resources :reviews, only: [:index, :create]
 end
